4530c62346e18f241481b8229765f1e51d0f1957
EC2 Default User <ec2-user@ip-172-31-43-212.ap-northeast-1.compute.internal>
2025-03-30 06:23:55 +0000

    new edition

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,13 @@
 class Comment < ApplicationRecord
   validates :content, presence: true, length: { maximum: 300 }
+  validate :no_forbidden_keywords  # 👈 添加这一行
+
+  private
+
+  def no_forbidden_keywords
+    banned_words = ["傻逼", "垃圾", "fuck", "操", "滚", "你妈", "敏感词"]  # 你可以添加更多词
+    if banned_words.any? { |word| content&.include?(word) }
+      errors.add(:content, "中包含违禁词")
+    end
+  end
 end
