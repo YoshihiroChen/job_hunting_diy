e46212eda96dbabd80057c4af8c36d9576aac7c6
EC2 Default User <ec2-user@ip-172-31-43-212.ap-northeast-1.compute.internal>
2025-05-08 15:27:16 +0000

    new edition

--- app/controllers/admin/job_roles_controller.rb
+++ app/controllers/admin/job_roles_controller.rb
@@ -4,8 +4,23 @@ module Admin
     http_basic_authenticate_with name: "admin", password: "yourpassword" if Rails.env.production?
 
     def index
-      @job_roles = JobRole.all.order(:industry, :role)
+      @job_roles = JobRole.all
+    
+      if params[:industry].present?
+        @job_roles = @job_roles.where(industry: params[:industry])
+      end
+    
+      if params[:literature_ok].present?
+        @job_roles = @job_roles.where(literature_ok: params[:literature_ok])
+      end
+    
+      if params[:trait].present?
+        @job_roles = @job_roles.where("traits LIKE ?", "%#{params[:trait]}%")
+      end
+    
+      
     end
+    
 
     def edit
       @job_role = JobRole.find(params[:id])
